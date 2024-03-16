efda005544e99c549b7c28ab1a38e5e5fc60e4c8
data-0101 <yo_harsh@outlook.com>
2024-03-16 18:12:25 +0530

    Added static and templates

--- project/news_app/views.py
+++ project/news_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home_page(request):
+    return render(request, "home.html")
+
+def news_app(request):
+    return render(request, 'index.html')
